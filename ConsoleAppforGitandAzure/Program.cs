85052c83448616026f87dbdd5c718675f2a9b60a
Saurabh Mishra <backtohome924@gmail.com>
2020-07-29 10:34:55 +0530

    new Project

---
+++ ConsoleAppforGitandAzure/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppforGitandAzure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sample project for Azure and Git");
+        }
+    }
+}
